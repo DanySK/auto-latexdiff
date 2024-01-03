976fce79afc499b37f418d3c3e7acf1028af5bc6
Danilo Pianini <danilo.pianini@unibo.it>
2024-01-03 14:58:00 +0100

    feat: enable the release on dockerhub

--- release.config.js
+++ release.config.js
@@ -1,5 +1,16 @@
+var prepareCmd = `
+docker build -t danysk/auto-latexdiff:latest . || exit 1
+`
+var publishCmd = `
+docker build -t danysk/auto-latexdiff:\${nextRelease.version} . || exit 1
+docker push --all-tags danysk/auto-latexdiff || exit 2
+`
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
+    ["@semantic-release/exec", {
+        "prepareCmd": prepareCmd,
+        "publishCmd": publishCmd,
+    }],
     "@semantic-release/github",
     "@semantic-release/git",
 )
